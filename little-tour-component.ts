96a636e5624bcc02f96629efaa919f2e109d18ed
Manasseh Ziki <32875198+oneziki@users.noreply.github.com>
2021-08-22 15:40:43 +0200

    Random Select Update
    One line solution and having ul to ol

--- little-tour-component.ts
+++ little-tour-component.ts
@@ -1,22 +1,25 @@
-import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-little-tour',
   template: `
-    <input #newUser
+    <input
+      #newUser
       (keyup.enter)="addUser(newUser.value)"
-      (blur)="addUser(newUser.value); newUser.value='' ">
+      (blur)="addUser(newUser.value); newUser.value = ''"
+    />
 
     <button (click)="addUser(newUser.value)">Add</button>
 
-    <ul><li *ngFor="let User of Users">{{User}}</li></ul>
+    <ol>
+      <li *ngFor="let User of Users">{{ User }}</li>
+    </ol>
   `
 })
 export class LittleTourComponent {
-  Users = ['Clicker Dance', 'Jerusalema', 'Thriller', 'Fire Ball'];
+  Users = ["Clickatell Dance", "Jerusalema", "To the Moon", "Thriller", "Forever Young", "Encho", "Buffalo Soldier"];
+  random = Math.floor(Math.random() * this.Users.length);
   addUser(newUser: string) {
     if (newUser) {
-      this.Users.push(newUser + "'s birthday song is " + this.Users[0]) ;
+      this.Users.push(newUser + "'s happy birthday song is number " + this.random);
     }
   }
 }
